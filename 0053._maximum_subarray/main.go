d5ece64e0b502c95cb43a4c3d14a4936086f3b60
JustForFun <heguozhuchn@gmail.com>
2020-06-12 22:40:07 +0800

    53. Maximum Subarray

--- 0053._maximum_subarray/main.go
+++ 0053._maximum_subarray/main.go
@@ -40,36 +40,9 @@ func maxSubArray(nums []int) (maxSum int) {
 	return maxSum
 }
 
-func maxSubArraySolutionII(nums []int) (maxSum int) {
-	if len(nums) == 0 {
-		// do we really need to consider 0 as largest sum of Nil array?
-		return 0
-	}
-	nowSum := nums[0]
-	for _, num := range nums[1:] {
-		if nowSum < 0 {
-			nowSum = num
-		} else {
-			nowSum += num
-		}
-
-		if maxSum < nowSum {
-			maxSum = nowSum
-		}
-
-	}
-	return maxSum
-}
-
 // test case
 func main() {
 	result := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
-	if reflect.DeepEqual(result, 6) != true {
-		panic(fmt.Sprintf("test case fail, result: %+v", result))
-	}
-
-	// Follow up case
-	result = maxSubArraySolutionII([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	if reflect.DeepEqual(result, 6) != true {
 		panic(fmt.Sprintf("test case fail, result: %+v", result))
 	}
