85833a165b471bce7d371c74af3970841b240d8c
JustForFun <heguozhuchn@gmail.com>
2020-06-30 12:59:23 +0800

    72. Edit Distance

---
+++ 0072_edit_distance/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+/*
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+	Insert a character
+	Delete a character
+	Replace a character
+
+Example 1:
+	Input: word1 = "horse", word2 = "ros"
+	Output: 3
+	Explanation:
+		horse -> rorse (replace 'h' with 'r')
+		rorse -> rose (remove 'r')
+		rose -> ros (remove 'e')
+
+Example 2:
+	Input: word1 = "intention", word2 = "execution"
+	Output: 5
+	Explanation:
+		intention -> inention (remove 't')
+		inention -> enention (replace 'i' with 'e')
+		enention -> exention (replace 'n' with 'x')
+		exention -> exection (replace 'n' with 'c')
+		exection -> execution (insert 'u')
+*/
+
+const UnKnow = -1
+
+func minDistance(word1 string, word2 string) (min int) {
+	mem := make([][]int, len(word1))
+	for i := range mem {
+		mem[i] = make([]int, len(word2))
+		for j := range mem[i] {
+			mem[i][j] = UnKnow
+		}
+	}
+	return minDistanceHelper(word1, 0, word2, 0, mem)
+}
+
+func minDistanceHelper(word1 string, word1Idx int, word2 string, word2Idx int, mem [][]int) (min int) {
+	if len(word1) == word1Idx {
+		return len(word2) - word2Idx
+	}
+
+	if len(word2) == word2Idx {
+		return len(word1) - word1Idx
+	}
+
+	if mem[word1Idx][word2Idx] != UnKnow {
+		return mem[word1Idx][word2Idx]
+	}
+
+	if word1[word1Idx] == word2[word2Idx] {
+		mem[word1Idx][word2Idx] = minDistanceHelper(word1, word1Idx+1, word2, word2Idx+1, mem)
+		return mem[word1Idx][word2Idx]
+	}
+
+	// Insert a character
+	min = 1 + minDistanceHelper(word1, word1Idx, word2, word2Idx+1, mem)
+	// Delete a character
+	nowMin := 1 + minDistanceHelper(word1, word1Idx+1, word2, word2Idx, mem)
+	if nowMin < min {
+		min = nowMin
+	}
+	// Replace a character
+	nowMin = 1 + minDistanceHelper(word1, word1Idx+1, word2, word2Idx+1, mem)
+	if nowMin < min {
+		min = nowMin
+	}
+
+	mem[word1Idx][word2Idx] = min
+	return mem[word1Idx][word2Idx]
+}
+
+// test case
+func main() {
+	result := minDistance("horse", "ros")
+	if reflect.DeepEqual(result, 3) != true {
+		panic(fmt.Sprintf("test case fail, result: %+v", result))
+	}
+
+	result = minDistance("intention", "execution")
+	if reflect.DeepEqual(result, 5) != true {
+		panic(fmt.Sprintf("test case fail, result: %+v", result))
+	}
+}
